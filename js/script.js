aada12a755fa4bba2ebd2b1c8b67ba8990b44671
likaburmak <burmakova93@list.ru>
2023-04-17 21:40:29 +0300

    completed task 2

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ let a=10;
 alert (a);
 a=20;
 alert (a);
+
+//task 2
+
+let yearProductionFirstIphone=2007;
+// yearProductionFirstIphone=prompt (‘Год выпуска первого iPhone?’);
+alert (yearProductionFirstIphone);
