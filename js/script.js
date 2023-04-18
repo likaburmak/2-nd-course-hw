957fcd6c23ddc83994bff38dcbf91aa0fc3ce485
likaburmak <burmakova93@list.ru>
2023-04-18 21:26:45 +0300

    task 4

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 //task 1
 
-let a=10;
-alert (a);
-a=20;
-alert (a);
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
 
 //task 2
 
-let yearProductionFirstIphone=2007;
+const yearProductionFirstIphone = 2007;
 // yearProductionFirstIphone=prompt (‘Год выпуска первого iPhone?’);
-alert (yearProductionFirstIphone);
+alert(yearProductionFirstIphone);
+
+//task 3
+const nameCreatorJavaScript = "Брендан Эйх";
+// nameCreatorJavaScript  = prompt (‘Имя создателя языка JavaScript?’);
+alert(nameCreatorJavaScript);
+
+//task 4
+let x = Number(10);
+let y = Number(2);
+alert(x+y);
+alert(x-y);
+alert(x*y);
+alert(x/y);
+
