5e4976214f0b9687eab3b6addf734e14713b6cfa
likaburmak <burmakova93@list.ru>
2023-04-23 22:20:13 +0300

    task 2

--- js-3/script.js
+++ js-3/script.js
@@ -5,3 +5,11 @@ if (password === 'skypro') {
 } else {
     alert('Пароль введен неправильно');
 }
+
+//task 2
+let c = Number(2);
+if (c>0 && c<10) {
+    console.log ('Верно');
+} else {
+    console.log ('Неверно');
+}
